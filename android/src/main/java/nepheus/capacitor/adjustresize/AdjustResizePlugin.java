ef8080b2c4940a973e43674ad54b5bedc6ff11eb
Jan Gessinger <jan@vi-a.de>
2023-03-05 10:43:07 +0100

    Added support for landscape orientation

--- android/src/main/java/nepheus/capacitor/adjustresize/AdjustResizePlugin.java
+++ android/src/main/java/nepheus/capacitor/adjustresize/AdjustResizePlugin.java
@@ -4,7 +4,6 @@ import android.graphics.Rect;
 import android.os.Build;
 import android.view.View;
 import android.view.ViewGroup;
-import android.view.ViewTreeObserver;
 
 import com.getcapacitor.Plugin;
 import com.getcapacitor.annotation.CapacitorPlugin;
@@ -13,36 +12,31 @@ import com.getcapacitor.annotation.CapacitorPlugin;
 public class AdjustResizePlugin extends Plugin {
     private View decorView;
     private View contentView;
-    private int additionalSpacing = -1;
+    private int additionalSpacing;
+    private int lastOrientation = -1;
 
     public void load() {
-        //only required on newer android versions. it was working on API level 19 (Build.VERSION_CODES.KITKAT)
         if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.KITKAT) {
             this.decorView = getActivity().getWindow().getDecorView();
             this.contentView = ((ViewGroup) decorView.findViewById(android.R.id.content)).getChildAt(0);
-            decorView.getViewTreeObserver().addOnGlobalLayoutListener(onGlobalLayoutListener);
+
+            decorView.getViewTreeObserver().addOnGlobalLayoutListener(() -> {
+                Rect r = new Rect();
+                decorView.getWindowVisibleDisplayFrame(r);
+
+                int height = decorView.getContext().getResources().getDisplayMetrics().heightPixels;
+                int orientation = getActivity().getResources().getConfiguration().orientation;
+
+                if (orientation != lastOrientation) {
+                    lastOrientation = orientation;
+                    additionalSpacing = (height - r.bottom) * -1;
+                }
+
+                int diff = height + additionalSpacing - r.bottom;
+                if (contentView.getPaddingBottom() != diff) {
+                    contentView.setPadding(0, 0, 0, diff);
+                }
+            });
         }
     }
-
-    ViewTreeObserver.OnGlobalLayoutListener onGlobalLayoutListener = new ViewTreeObserver.OnGlobalLayoutListener() {
-        @Override
-        public void onGlobalLayout() {
-            //r will be populated with the coordinates of your view that area still visible.
-            Rect r = new Rect();
-            decorView.getWindowVisibleDisplayFrame(r);
-
-            //get screen height and calculate the difference with the useable area from the r
-            int height = decorView.getContext().getResources().getDisplayMetrics().heightPixels;
-
-            if (additionalSpacing == -1) {
-                //get additional spacing that could be caused by elements like the status bar
-                additionalSpacing = (height - r.bottom) * -1;
-            }
-
-            int diff = height + additionalSpacing - r.bottom;
-            if (contentView.getPaddingBottom() != diff) {
-                contentView.setPadding(0, 0, 0, diff);
-            }
-        }
-    };
 }
